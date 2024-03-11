268ad141abee78e4baf24b2c4bbbfe3fc11ae0d6
Kyle Lin <kylejlin.us@gmail.com>
2024-03-11 23:12:09 +0900

    Add textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface State {
+  readonly code: string;
 }
 
-export default App;
+export class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      code: "",
+    };
+
+    this.bindMethods();
+  }
+
+  bindMethods() {
+    this.onCodeChanged = this.onCodeChanged.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <textarea
+          value={this.state.code}
+          onChange={this.onCodeChanged}
+        ></textarea>
+      </div>
+    );
+  }
+
+  onCodeChanged(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({ code: event.target.value });
+  }
+}
