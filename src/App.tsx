65fe5a3d19e4eb8cba680f81f1c80ea0e67e33e9
Kyle Lin <kylejlin.us@gmail.com>
2024-03-12 00:04:04 +0900

    Start working on AST

--- src/App.tsx
+++ src/App.tsx
@@ -41,3 +41,95 @@ export class App extends React.Component<{}, State> {
 function highlight(code: string): React.ReactElement[] {
   return [<span>{code}</span>];
 }
+
+interface Doc {
+  defs: Def;
+}
+
+type Def = QueryDef | CommandDef;
+
+enum DefKind {
+  Query,
+  Command,
+}
+
+interface QueryDef {
+  kind: DefKind.Query;
+  signature: SignaturePart[];
+  body: Expression[];
+  span: Span;
+}
+
+interface CommandDef {
+  kind: DefKind.Command;
+  signature: SignaturePart[];
+  body: Expression[];
+  span: Span;
+}
+
+type SignaturePart = Label | Param;
+
+interface Label {
+  isLabel: true;
+  name: Phrase;
+}
+
+interface Phrase {
+  normalizedValue: string;
+  span: Span;
+}
+
+enum Type {
+  Number,
+  String,
+  Boolean,
+  NumberListRef,
+  StringListRef,
+  BooleanListRef,
+  Command,
+}
+
+interface Param {
+  isLabel: false;
+  name: Phrase;
+  type: Type;
+  span: Span;
+}
+
+interface Command {
+  parts: ExpressionPart[];
+  semicolonStart: TextPosition;
+}
+
+interface Expression {
+  parts: ExpressionPart[];
+}
+
+type ExpressionPart = Label | Subexpression;
+
+interface Subexpression {
+  isLabel: false;
+  expression: Expression;
+  bracketKind: SubexpressionBracketKind;
+  span: Span;
+}
+
+enum SubexpressionBracketKind {
+  Paren = "paren",
+  Square = "square",
+}
+
+interface Span {
+  start: TextPosition;
+  end: TextPosition;
+}
+
+interface TextPosition {
+  byteIndex: number;
+  line: number;
+  column: number;
+}
+
+function parse(code: string): Doc {
+  throw new Error("Todo");
+}
