fe3cd812fc0f2a4e3468429e9f541f31c67373a0
Kyle Lin <kylejlin.us@gmail.com>
2024-04-01 19:44:49 +0900

    Empty top bar

--- src/App.tsx
+++ src/App.tsx
@@ -105,6 +105,7 @@ export class App extends React.Component<{}, State> {
   render() {
     return (
       <div className="App">
+        <div className="TopBar"></div>
         <div className="LeftPanel">
           {/* The design for the CodeInput is inspired by https://codersblock.com/blog/highlight-text-inside-a-textarea/ */}
           <div className="CodeInput__Container">
