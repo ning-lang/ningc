4f98edbe45d711369a2189c495675baa2fd8c043
Kyle Lin <kylejlin.us@gmail.com>
2024-04-06 01:07:05 +0900

    Make UNTYPED_BUILTINS[number].signature a string

--- src/untypedBuiltins.ts
+++ src/untypedBuiltins.ts
@@ -2,411 +2,207 @@ export const UNTYPED_VAL_SENTINEL = "()";
 export const UNTYPED_REF_SENTINEL = "[]";
 export const UNTYPED_BLOCK_SENTINEL = "{}";
 
-const VAL = UNTYPED_VAL_SENTINEL;
-const REF = UNTYPED_REF_SENTINEL;
-const BLOCK = UNTYPED_BLOCK_SENTINEL;
-
 export const UNTYPED_BUILTINS = {
   // Control
+
   if_: {
     isQuery: false,
-    signature: ["if", VAL, BLOCK],
+    signature: "if () {}",
   },
   ifElse: {
     isQuery: false,
-    signature: ["if", VAL, BLOCK, "else", BLOCK],
+    signature: "if () {} else {}",
   },
   while_: {
     isQuery: false,
-    signature: ["while", VAL, BLOCK],
+    signature: "while () {}",
   },
   repeat: {
     isQuery: false,
-    signature: ["repeat", VAL, "times", BLOCK],
+    signature: "repeat () times {}",
   },
   valReturn: {
     isQuery: false,
-    signature: ["return", VAL],
+    signature: "return ()",
   },
   voidReturn: {
     isQuery: false,
-    signature: ["return"],
+    signature: "return",
   },
 
   // Variables
+
   let_: {
     isQuery: false,
-    signature: ["let", REF, "=", VAL],
+    signature: "let [] = ()",
   },
   var_: {
     isQuery: false,
-    signature: ["var", REF, "=", VAL],
+    signature: "var [] = ()",
   },
   assign: {
     isQuery: false,
-    signature: ["set", REF, "to", VAL],
+    signature: "set [] to ()",
   },
   increase: {
     isQuery: false,
-    signature: ["increase", REF, "by", VAL],
+    signature: "increase [] by ()",
   },
 
   // Lists
+
   numberListCreate: {
     isQuery: false,
-    signature: ["create number list", REF],
+    signature: "create number list []",
   },
   stringListCreate: {
     isQuery: false,
-    signature: ["create string list", REF],
+    signature: "create string list []",
   },
   booleanListCreate: {
     isQuery: false,
-    signature: ["create boolean list", REF],
+    signature: "create boolean list []",
   },
 
   listLength: {
     isQuery: true,
-    signature: ["length of", REF],
+    signature: "length of []",
   },
 
   listItemOf: {
     isQuery: true,
-    signature: ["item", VAL, "of", REF],
+    signature: "item () of []",
   },
 
   listOrIndexOf: {
     isQuery: true,
-    signature: ["index of", VAL, "in", REF],
+    signature: "index of () in []",
   },
 
   listContains: {
     isQuery: true,
-    signature: [REF, "contains", VAL, "?"],
+    signature: "[] contains ()?",
   },
 
   listReplaceItem: {
     isQuery: false,
-    signature: ["replace item", VAL, "of", REF, "with", VAL],
+    signature: "replace item () of [] with ()",
   },
   listInsert: {
     isQuery: false,
-    signature: ["insert", VAL, "at", VAL, "of", REF],
+    signature: "insert () at () of []",
   },
   listDeleteItem: {
     isQuery: false,
-    signature: ["delete item", VAL, "of", REF],
+    signature: "delete item () of []",
   },
   listDeleteAll: {
     isQuery: false,
-    signature: ["delete all of", REF],
+    signature: "delete all of []",
   },
   listAdd: {
     isQuery: false,
-    signature: ["add", VAL, "to", REF],
+    signature: "add () to []",
   },
 
   // Operators
-  opAdd: {
-    isQuery: true,
-    signature: [VAL, "+", VAL],
-  },
-  opSub: {
-    isQuery: true,
-    signature: [VAL, "-", VAL],
-  },
-  opMul: {
-    isQuery: true,
-    signature: [VAL, "*", VAL],
-  },
-  opDiv: {
-    isQuery: true,
-    signature: [VAL, "/", VAL],
-  },
-  opMod: {
-    isQuery: true,
-    signature: [VAL, "mod", VAL],
-  },
-  opPow: {
-    isQuery: true,
-    signature: [VAL, "to the power of", VAL],
-  },
 
-  opEq: {
-    isQuery: true,
-    signature: [VAL, "==", VAL],
-  },
-  opNe: {
-    isQuery: true,
-    signature: [VAL, "!=", VAL],
-  },
-  opLt: {
-    isQuery: true,
-    signature: [VAL, "<", VAL],
-  },
-  opLe: {
-    isQuery: true,
-    signature: [VAL, "<=", VAL],
-  },
-  opGt: {
-    isQuery: true,
-    signature: [VAL, ">", VAL],
-  },
-  opGe: {
-    isQuery: true,
-    signature: [VAL, ">=", VAL],
-  },
+  opAdd: { isQuery: true, signature: "() + ()" },
+  opSub: { isQuery: true, signature: "() - ()" },
+  opMul: { isQuery: true, signature: "() * ()" },
+  opDiv: { isQuery: true, signature: "() / ()" },
+  opMod: { isQuery: true, signature: "() mod ()" },
+  opPow: { isQuery: true, signature: "() to the power of ()" },
 
-  opExp: {
-    isQuery: true,
-    signature: ["exp", VAL],
-  },
-  opLn: {
-    isQuery: true,
-    signature: ["ln", VAL],
-  },
-  opSinRad: {
-    isQuery: true,
-    signature: ["sin of", VAL, "radians"],
-  },
-  opCosRad: {
-    isQuery: true,
-    signature: ["cos of", VAL, "radians"],
-  },
-  opTanRad: {
-    isQuery: true,
-    signature: ["tan of", VAL, "radians"],
-  },
-  opAsinRad: {
-    isQuery: true,
-    signature: ["asin of", VAL, "in radians"],
-  },
-  opAcosRad: {
-    isQuery: true,
-    signature: ["acos of", VAL, "in radians"],
-  },
-  opAtanRad: {
-    isQuery: true,
-    signature: ["atan of", VAL, "in radians"],
-  },
-  opAtan2Rad: {
-    isQuery: true,
-    signature: ["atan2 of y", VAL, "x", VAL, "in radians"],
-  },
-  opPi: {
-    isQuery: true,
-    signature: ["pi"],
-  },
-  opNaN: {
-    isQuery: true,
-    signature: ["NaN"],
-  },
-  opInfinity: {
-    isQuery: true,
-    signature: ["Infinity"],
-  },
-  opNegInfinity: {
-    isQuery: true,
-    signature: ["-Infinity"],
-  },
+  opEq: { isQuery: true, signature: "() == ()" },
+  opNe: { isQuery: true, signature: "() != ()" },
+  opLt: { isQuery: true, signature: "() < ()" },
+  opLe: { isQuery: true, signature: "() <= ()" },
+  opGt: { isQuery: true, signature: "() > ()" },
+  opGe: { isQuery: true, signature: "() >= ()" },
 
-  opFloor: {
-    isQuery: true,
-    signature: ["floor", VAL],
-  },
-  opCeil: {
-    isQuery: true,
-    signature: ["ceiling", VAL],
-  },
-  opRound: {
-    isQuery: true,
-    signature: ["round", VAL],
-  },
-  opAbs: {
-    isQuery: true,
-    signature: ["abs", VAL],
-  },
-  opMin: {
-    isQuery: true,
-    signature: ["min of", VAL, "and", VAL],
-  },
-  opMax: {
-    isQuery: true,
-    signature: ["max of", VAL, "and", VAL],
-  },
+  opExp: { isQuery: true, signature: "exp ()" },
+  opLn: { isQuery: true, signature: "ln ()" },
+  opSinRad: { isQuery: true, signature: "sin of () radians" },
+  opCosRad: { isQuery: true, signature: "cos of () radians" },
+  opTanRad: { isQuery: true, signature: "tan of () radians" },
+  opAsinRad: { isQuery: true, signature: "asin of () in radians" },
+  opAcosRad: { isQuery: true, signature: "acos of () in radians" },
+  opAtanRad: { isQuery: true, signature: "atan of () in radians" },
+  opAtan2Rad: { isQuery: true, signature: "atan2 of y () x () in radians" },
+  opPi: { isQuery: true, signature: "pi" },
+  opNaN: { isQuery: true, signature: "NaN" },
+  opInfinity: { isQuery: true, signature: "Infinity" },
+  opNegInfinity: { isQuery: true, signature: "-Infinity" },
 
-  opAnd: {
-    isQuery: true,
-    signature: [VAL, "and", VAL],
-  },
-  opOr: {
-    isQuery: true,
-    signature: [VAL, "or", VAL],
-  },
-  opNot: {
-    isQuery: true,
-    signature: ["not", VAL],
-  },
+  opFloor: { isQuery: true, signature: "floor ()" },
+  opCeil: { isQuery: true, signature: "ceiling ()" },
+  opRound: { isQuery: true, signature: "round ()" },
+  opAbs: { isQuery: true, signature: "abs ()" },
+  opMin: { isQuery: true, signature: "min of () and ()" },
+  opMax: { isQuery: true, signature: "max of () and ()" },
 
-  opConcat: {
-    isQuery: true,
-    signature: [VAL, "++", VAL],
-  },
-  stringLength: {
-    isQuery: true,
-    signature: ["length of", VAL],
-  },
-  stringLetter: {
-    isQuery: true,
-    signature: ["letter", VAL, "of", VAL],
-  },
+  opAnd: { isQuery: true, signature: "() and ()" },
+  opOr: { isQuery: true, signature: "() or ()" },
+  opNot: { isQuery: true, signature: "not ()" },
+
+  opConcat: { isQuery: true, signature: "() ++ ()" },
+  stringLength: { isQuery: true, signature: "length of ()" },
+  stringLetter: { isQuery: true, signature: "letter () of ()" },
   stringSubstring: {
     isQuery: true,
-    signature: ["substring of", VAL, "from", VAL, "to", VAL],
+    signature: "substring of () from () to ()",
   },
-  stringContains: {
-    isQuery: true,
-    signature: [VAL, "contains", VAL, "?"],
-  },
-  stringIndexOf: {
-    isQuery: true,
-    signature: ["index of", VAL, "in", VAL],
-  },
+  stringContains: { isQuery: true, signature: "() contains ()?" },
+  stringIndexOf: { isQuery: true, signature: "index of () in ()" },
 
-  ternary: {
-    isQuery: true,
-    signature: ["if", VAL, "then", VAL, "else", VAL],
-  },
+  ternary: { isQuery: true, signature: "if () then () else ()" },
 
-  parseNumber: {
-    isQuery: true,
-    signature: ["parse", VAL, "as number"],
-  },
+  parseNumber: { isQuery: true, signature: "parse () as number" },
 
-  numberOrBooleanToString: {
-    isQuery: true,
-    signature: ["convert", VAL, "to string"],
-  },
+  numberOrBooleanToString: { isQuery: true, signature: "convert () to string" },
 
-  randomInt: {
-    isQuery: true,
-    signature: ["random integer from", VAL, "up to but not including", VAL],
-  },
+  randomInt: { isQuery: true, signature: "random integer from () to ()" },
 
   // Sensing
-  windowMouseX: {
-    isQuery: true,
-    signature: ["window mouse x"],
-  },
-  windowMouseY: {
-    isQuery: true,
-    signature: ["window mouse y"],
-  },
-  canvasMouseX: {
-    isQuery: true,
-    signature: ["canvas mouse x"],
-  },
-  canvasMouseY: {
-    isQuery: true,
-    signature: ["canvas mouse y"],
-  },
-  mouseDown: {
-    isQuery: true,
-    signature: ["mouse down?"],
-  },
 
-  windowWidth: {
-    isQuery: true,
-    signature: ["window width"],
-  },
-  windowHeight: {
-    isQuery: true,
-    signature: ["window height"],
-  },
-  canvasWidth: {
-    isQuery: true,
-    signature: ["canvas width"],
-  },
-  canvasHeight: {
-    isQuery: true,
-    signature: ["canvas height"],
-  },
+  windowMouseX: { isQuery: true, signature: "window mouse x" },
+  windowMouseY: { isQuery: true, signature: "window mouse y" },
+  canvasMouseX: { isQuery: true, signature: "canvas mouse x" },
+  canvasMouseY: { isQuery: true, signature: "canvas mouse y" },
+  mouseDown: { isQuery: true, signature: "mouse down?" },
+
+  windowWidth: { isQuery: true, signature: "window width" },
+  windowHeight: { isQuery: true, signature: "window height" },
+  canvasWidth: { isQuery: true, signature: "canvas width" },
+  canvasHeight: { isQuery: true, signature: "canvas height" },
 
   millisecondsSinceUnixEpoch: {
     isQuery: true,
-    signature: ["milliseconds since unix epoch"],
+    signature: "milliseconds since unix epoch",
   },
-  currentYear: {
-    isQuery: true,
-    signature: ["current year"],
-  },
-  currentMonth: {
-    isQuery: true,
-    signature: ["current month"],
-  },
-  currentDate: {
-    isQuery: true,
-    signature: ["current date"],
-  },
-  currentDayOfWeek: {
-    isQuery: true,
-    signature: ["current day of the week"],
-  },
-  currentHour: {
-    isQuery: true,
-    signature: ["current hour"],
-  },
-  currentMinute: {
-    isQuery: true,
-    signature: ["current minute"],
-  },
-  currentSecond: {
-    isQuery: true,
-    signature: ["current second"],
-  },
+  currentYear: { isQuery: true, signature: "current year" },
+  currentMonth: { isQuery: true, signature: "current month" },
+  currentDate: { isQuery: true, signature: "current date" },
+  currentDayOfWeek: { isQuery: true, signature: "current day of the week" },
+  currentHour: { isQuery: true, signature: "current hour" },
+  currentMinute: { isQuery: true, signature: "current minute" },
+  currentSecond: { isQuery: true, signature: "current second" },
 
-  keyPressed: {
-    isQuery: true,
-    signature: ["key", VAL, "pressed?"],
-  },
+  keyPressed: { isQuery: true, signature: "key () pressed?" },
 
   // Looks
+
   resizeCanvas: {
     isQuery: false,
-    signature: [
-      "resize canvas to width",
-      VAL,
-      "height",
-      VAL,
-      "and erase everything",
-    ],
+    signature: "resize canvas to width () height () and erase everything",
   },
   drawImage: {
     isQuery: false,
-    signature: [
-      "draw image",
-      VAL,
-      "at x",
-      VAL,
-      "y",
-      VAL,
-      "with width",
-      VAL,
-      "height",
-      VAL,
-    ],
+    signature: "draw image () at x () y () with width () height ()",
   },
   clearRect: {
     isQuery: false,
-    signature: [
-      "erase rectangle at x",
-      VAL,
-      "y",
-      VAL,
-      "width",
-      VAL,
-      "height",
-      VAL,
-    ],
+    signature: "erase rectangle at x () y () width () height ()",
   },
 } as const;
