5f71f0514b585598ccc0f08770d9644559078d41
Kyle Lin <kylejlin.us@gmail.com>
2024-04-18 19:53:14 +0900

    Impl stringifyCommand

--- src/stringifyNingNode.ts
+++ src/stringifyNingNode.ts
@@ -1,8 +1,67 @@
 import { stringifyIdentifierSequence } from "./funcSignature";
 import type * as ast from "./types/tysonTypeDict";
 
+export const INDENT_SIZE = 4;
+
 export function stringifyCommand(command: ast.Command): string {
-  return "TODO IMPLEMENT stringifyCommand;";
+  return stringifyCommandWithIndent(command, 0);
+}
+
+export function stringifyCommandWithIndent(
+  command: ast.Command,
+  indentSpaces: number
+): string {
+  const indent = " ".repeat(indentSpaces);
+  return (
+    indent +
+    command.parts.map((p) => stringifyCommandPart(p, indentSpaces)).join(" ") +
+    ";"
+  );
+}
+
+function stringifyCommandPart(
+  part: ast.CommandPart,
+  indentSpaces: number
+): string {
+  if (part.kind === "identifier") {
+    return part.name;
+  }
+
+  if (part.kind === "parenthesized_expression") {
+    return "(" + stringifyExpression(part.expression) + ")";
+  }
+
+  if (part.kind === "square_bracketed_identifier_sequence") {
+    return "[" + stringifyIdentifierSequence(part.identifiers) + "]";
+  }
+
+  if (part.kind === "block_command") {
+    return stringifyBlockCommand(part, indentSpaces);
+  }
+
+  const _exhaustivnessCheck: never = part;
+  return _exhaustivnessCheck;
+}
+
+function stringifyBlockCommand(
+  command: ast.BlockCommand,
+  indentSpaces: number
+): string {
+  const indent = " ".repeat(indentSpaces);
+
+  let out = "{";
+
+  for (const c of command.commands) {
+    out += "\n" + stringifyCommandWithIndent(c, indentSpaces + INDENT_SIZE);
+  }
+
+  if (command.commands.length === 0) {
+    out += "}";
+  } else {
+    out += "\n" + indent + "}";
+  }
+
+  return out;
 }
 
 export function stringifyExpression(expr: ast.Expression): string {
