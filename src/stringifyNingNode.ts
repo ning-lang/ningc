115fe2594b9ac6b36523fe4c119e971ddc7c1a3c
Kyle Lin <kylejlin.us@gmail.com>
2024-04-18 19:45:50 +0900

    Impl stringifyExpression

--- src/stringifyNingNode.ts
+++ src/stringifyNingNode.ts
@@ -1,3 +1,4 @@
+import { stringifyIdentifierSequence } from "./funcSignature";
 import type * as ast from "./types/tysonTypeDict";
 
 export function stringifyCommand(command: ast.Command): string {
@@ -5,5 +6,31 @@ export function stringifyCommand(command: ast.Command): string {
 }
 
 export function stringifyExpression(expr: ast.Expression): string {
-  return "TODO IMPLEMENT stringifyExpression";
+  if (expr.kind === "string_literal") {
+    return expr.source;
+  }
+  return stringifyCompoundExpression(expr);
+}
+
+function stringifyCompoundExpression(expr: ast.CompoundExpression): string {
+  return expr.parts.map(stringifyCompoundExpressionPart).join(" ");
+}
+
+function stringifyCompoundExpressionPart(
+  part: ast.CompoundExpressionPart
+): string {
+  if (part.kind === "identifier") {
+    return part.name;
+  }
+
+  if (part.kind === "parenthesized_expression") {
+    return "(" + stringifyExpression(part.expression) + ")";
+  }
+
+  if (part.kind === "square_bracketed_identifier_sequence") {
+    return "[" + stringifyIdentifierSequence(part.identifiers) + "]";
+  }
+
+  const _exhaustivnessCheck: never = part;
+  return _exhaustivnessCheck;
 }
