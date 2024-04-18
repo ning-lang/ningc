ec01af25567f01acf7620eaf0f22a8ff30906661
Kyle Lin <kylejlin.us@gmail.com>
2024-04-19 01:59:22 +0900

    Update hello world code

--- src/helloWorldCode.ts
+++ src/helloWorldCode.ts
@@ -1,7 +1,4 @@
-export const HELLO_WORLD_CODE = `// Write comments with two slashes,
-// like this.
-
-// Declare global variables in the \`Global\` section:
+export const HELLO_WORLD_CODE = `// You can declare global variables in the \`Global\` section:
 Global {
     var [x] = (0);
     var [time] = (milliseconds since unix epoch);
@@ -15,12 +12,14 @@ Global {
 
 }
 
+// \`render\` is a special command that is called every frame.
 Command (render) {
     resize canvas to width (width) height (height) and erase everything;
     draw image ("background.png") at x (0) y (0) with width (width) height (height);
     draw image ("ball.png") at x (ball left) y (170) with width (ball diameter) height (ball diameter);
 }
 
+// \`update\` is a special command that is also called every frame.
 Command (update) {
     let [current time] = (milliseconds since unix epoch);
     let [elapsed time] = ((current time) - (time));
@@ -33,6 +32,16 @@ Command (update) {
         increase [ball left] by ((elapsed time) / (10));
     };
 
+    if (key ("left arrow") pressed?) {
+        reset ball
+    };
+
     set [time] to (current time);
+}
+
+// You can define your own commands.
+// For example, here's how you might define a \`reset ball\` command:
+Command (reset ball) {
+    set [ball left] to (230);
 }
 `;
